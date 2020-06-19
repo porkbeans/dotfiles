9f9f13f5c4c0392dbd8f9a3ae069fb98892ef09e
porkbeans <me@porkbeans.dev>
2020-06-19 10:11:29 +0900

    Fix exa options

--- exact_private_dot_rc.d/99_alias_core.sh
+++ exact_private_dot_rc.d/99_alias_core.sh
@@ -1,4 +1,4 @@
 alias ls='exa --group-directories-first'
 alias ll='exa --group-directories-first -l'
-alias la='exa --group-directories-first -a'
-alias lla='exa --group-directories-first -la'
+alias la='exa --group-directories-first -aa'
+alias lla='exa --group-directories-first -laa'
