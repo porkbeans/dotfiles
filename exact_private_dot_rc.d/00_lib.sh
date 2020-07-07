17e1da243d2525b9a55497cd9a9ac038a0987971
porkbeans <me@porkbeans.dev>
2020-07-07 21:37:22 +0900

    Remove powerline-go & Simplify shell detection

--- exact_private_dot_rc.d/00_lib.sh
+++ exact_private_dot_rc.d/00_lib.sh
@@ -1,4 +1,11 @@
 # shellcheck disable=SC2155
+if [ -n "$BASH_VERSION" ]; then
+  export SHELL_NAME="bash"
+elif [ -n "$ZSH_VERSION" ]; then
+  export SHELL_NAME="zsh"
+else
+  export SHELL_NAME="$(basename "${SHELL}")"
+fi
 
 exists_command() {
   command -v "$1" >/dev/null 2>&1
--- exact_private_dot_rc.d/99_autocomplete_minikube.sh
+++ exact_private_dot_rc.d/99_autocomplete_minikube.sh
@@ -1,3 +1,3 @@
 if exists_command minikube; then
-  eval "$(minikube completion "$(basename "${SHELL}")")"
+  eval "$(minikube completion "${SHELL_NAME}")"
 fi
