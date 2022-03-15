e032ff586d7b08ec94db9d76af1ae0e43a312b43
porkbeans <me@porkbeans.dev>
2022-03-15 11:26:49 +0900

    Add mkcd function

--- exact_private_dot_rc.d/99_alias_core.sh
+++ exact_private_dot_rc.d/99_alias_core.sh
@@ -1,3 +1,7 @@
 alias ls='exa --group-directories-first'
 alias la='exa --group-directories-first -aa'
 alias ll='exa --group-directories-first -lbgHaa'
+
+mkcd(){
+    mkdir -pv "$1" && cd "$1" || echo "mkcd: failed to create or enter directory '$1'"
+}
