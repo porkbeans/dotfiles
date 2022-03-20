6117d47450d4200ae607b7c564ff92dc60b933f4
porkbeans <me@porkbeans.dev>
2022-03-20 17:01:12 +0900

    Consider if exa not found

--- exact_private_dot_rc.d/99_alias_core.sh
+++ exact_private_dot_rc.d/99_alias_core.sh
@@ -1,6 +1,12 @@
-alias ls='exa --group-directories-first'
-alias la='exa --group-directories-first -aa'
-alias ll='exa --group-directories-first -lbgHaa'
+if has_command exa; then
+  alias ls='exa --group-directories-first'
+  alias la='exa --group-directories-first -aa'
+  alias ll='exa --group-directories-first -lbgHaa'
+else
+  alias ls='ls --group-directories-first'
+  alias la='ls --group-directories-first -a'
+  alias ll='ls --group-directories-first -la'
+fi
 
 mkcd(){
     mkdir -pv "$1" && cd "$1" || echo "mkcd: failed to create or enter directory '$1'"
