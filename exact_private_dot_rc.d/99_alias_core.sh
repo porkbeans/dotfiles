1b6c0c853639a8477dcd8d6889e66bc47aa43fd8
porkbeans <me@porkbeans.dev>
2022-12-02 23:57:22 +0900

    Add tree option for ls

--- exact_private_dot_rc.d/99_alias_core.sh
+++ exact_private_dot_rc.d/99_alias_core.sh
@@ -3,6 +3,7 @@ if has_command exa; then
   alias ls='exa --group-directories-first'
   alias la='exa --group-directories-first -aa'
   alias ll='exa --group-directories-first -lbgHaa'
+  alias llt='exa --group-directories-first -lbgHTa'
 else
   alias ls='ls --group-directories-first'
   alias la='ls --group-directories-first -a'
