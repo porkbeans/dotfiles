c00fc8b24a375463166346371e1877a57bbd84a0
porkbeans <me@porkbeans.dev>
2023-01-02 18:56:25 +0900

    Remove deprecated features

--- exact_private_dot_rc.d/10_node.sh
+++ exact_private_dot_rc.d/10_node.sh
@@ -3,17 +3,3 @@ if has_command volta; then
   export VOLTA_HOME="${HOME}/.volta"
   add_path "${VOLTA_HOME}/bin"
 fi
-
-if has_command npm; then
-  npm config set prefix "${HOME}/.npm" >/dev/null 2>&1
-
-  # shellcheck disable=SC2155
-  add_path "$(npm bin -g 2>/dev/null)"
-fi
-
-if has_command yarn; then
-  yarn config set prefix "${HOME}/.yarn" >/dev/null 2>&1
-
-  # shellcheck disable=SC2155
-  add_path "$(yarn global bin)"
-fi
