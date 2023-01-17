f0b24efb4042bdbb30e9d4ef574a71b632516ab1
porkbeans <me@porkbeans.dev>
2023-01-17 22:14:41 +0900

    Reformat and fix lxc image refresh

--- exact_private_dot_rc.d/01_container.sh
+++ exact_private_dot_rc.d/01_container.sh
@@ -2,20 +2,21 @@
 
 export DOCKER_CONTENT_TRUST=0
 
-docker-image-refresh(){
+docker-image-refresh() {
   docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -v '<none>' | xargs -I {} docker pull {}
   docker image prune -f
 }
 
-podman-image-refresh(){
+podman-image-refresh() {
   podman image ls --format '{{.Repository}}:{{.Tag}}' | grep -v '<none>' | xargs -I {} podman pull {}
   podman image prune -f
 }
 
-lxc-image-refresh(){
-  lxc image list -f json | jq -r '.[] | select(.aliases | length != 0) | .update_source.alias' | xargs -I {} lxc image refresh {}
+lxc-image-refresh() {
+  # shellcheck disable=SC2046
+  lxc image refresh $(lxc image list -f json | jq -r '.[].fingerprint')
 }
 
-lxc-image-pull(){
+lxc-image-pull() {
   lxc image copy --copy-aliases "images:$1" 'local:'
 }
