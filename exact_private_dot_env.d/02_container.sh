87bc176c80748bd6c723e6e3eebe58851771423d
porkbeans <me@porkbeans.dev>
2023-01-21 20:11:45 +0900

    Add desc column for lxc-image-list

--- exact_private_dot_env.d/02_container.sh
+++ exact_private_dot_env.d/02_container.sh
@@ -12,11 +12,18 @@ podman-image-refresh() {
   podman image prune -f
 }
 
-lxc-image-list() {
+lxc-image-ls() {
   lxc image list -f json |
     jq -r '["NAME", "TYPE", "SIZE", "FINGERPRINT"], ([.[] | {"name": .update_source.alias, type, size, fingerprint}] | sort_by(.name, .type) | .[] | [.name, .type, .size, .fingerprint]) | @tsv' |
     numfmt --to si --suffix B --field 3 --invalid ignore -d $'\t' |
     column -ts $'\t' -R 3
+}
+
+lxc-image-ll() {
+  lxc image list -f json |
+    jq -r '["NAME", "TYPE", "DESC", "SIZE", "FINGERPRINT"], ([.[] | {"name": .update_source.alias, type, "desc": .properties.description, size, fingerprint}] | sort_by(.name, .type) | .[] | [.name, .type, .desc, .size, .fingerprint]) | @tsv' |
+    numfmt --to si --suffix B --field 4 --invalid ignore -d $'\t' |
+    column -ts $'\t' -R 4
 }
 
 lxc-image-refresh() {
