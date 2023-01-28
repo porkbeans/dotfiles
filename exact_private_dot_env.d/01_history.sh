254c349bf3a7918a32a78a91d835ebea74ef4624
porkbeans <me@porkbeans.dev>
2023-01-28 14:41:00 +0900

    Fix hitory settings for zsh

--- exact_private_dot_env.d/01_history.sh
+++ exact_private_dot_env.d/01_history.sh
@@ -1,3 +1,15 @@
 # shellcheck shell=sh
-export HISTSIZE=10000
-export HISTFILESIZE=50000
+case "${SHELL_NAME}" in
+  bash)
+    export HISTFILE="${HOME}/.bash_history"
+    export HISTSIZE=10000
+    export HISTFILESIZE=50000
+    ;;
+  zsh)
+    export HISTFILE="${ZDOTDIR}/.zsh_history"
+    export SAVEHIST=10000
+    export HISTSIZE=10000
+    ;;
+  *)
+    ;;
+esac
