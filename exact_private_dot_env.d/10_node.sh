6dfd78a6b3b7a7bbcd3748a25acd090800a6b1cb
porkbeans <me@porkbeans.dev>
2023-07-22 14:35:11 +0900

    Add clean script for volta

--- exact_private_dot_env.d/10_node.sh
+++ exact_private_dot_env.d/10_node.sh
@@ -2,4 +2,16 @@
 if has_command volta; then
   export VOLTA_HOME="${HOME}/.volta"
   add_path "${VOLTA_HOME}/bin"
+
+  volta_clean_node(){
+    node_version="$1"
+
+    if [ -n "${tool_version}" ] && [ ! -d "${HOME}/.volta/tools/image/node/${node_version}/" ]; then
+      echo "node v${tool_version} not found"
+      return
+    fi
+
+    rm -rf "${HOME}/.volta/tools/image/node/${node_version}/"
+    rm "${HOME}/.volta/tools/inventory/node/node-v${node_version}"-*
+  }
 fi
