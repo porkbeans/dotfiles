67c89ee134359f1682f06ec00778c5cf99d238c9
porkbeans <me@porkbeans.dev>
2023-09-09 13:08:02 +0900

    Replace exa with eza

--- exact_private_dot_env.d/01_ls.sh
+++ exact_private_dot_env.d/01_ls.sh
@@ -1,11 +1,11 @@
 # shellcheck shell=sh
 export TIME_STYLE=long-iso
 
-if has_command exa; then
-  alias ls='exa --group-directories-first'
-  alias la='exa --group-directories-first -aa'
-  alias ll='exa --group-directories-first -lbgHaa'
-  alias llt='exa --group-directories-first -lbgHTa'
+if has_command eza; then
+  alias ls='eza --group-directories-first'
+  alias la='eza --group-directories-first -aa'
+  alias ll='eza --group-directories-first -lbgHaa'
+  alias llt='eza --group-directories-first -lbgHTa'
 else
   alias ls='ls --group-directories-first'
   alias la='ls --group-directories-first -a'
