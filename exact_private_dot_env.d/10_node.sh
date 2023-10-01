d32c7a768829eb5cd9af9fe188eaf9e684e00f2a
porkbeans <me@porkbeans.dev>
2023-10-01 19:12:28 +0900

    Fix function for removing old node versions

--- exact_private_dot_env.d/10_node.sh
+++ exact_private_dot_env.d/10_node.sh
@@ -6,8 +6,13 @@ if has_command volta; then
   volta_clean_node(){
     node_version="$1"
 
-    if [ -n "${tool_version}" ] && [ ! -d "${HOME}/.volta/tools/image/node/${node_version}/" ]; then
-      echo "node v${tool_version} not found"
+    if [ -z "${node_version}" ]; then
+      echo "Empty argument. Specify node version."
+      return
+    fi
+
+    if [ ! -d "${HOME}/.volta/tools/image/node/${node_version}/" ]; then
+      echo "node v${node_version} not found"
       return
     fi
 
