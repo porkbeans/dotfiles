8fe8598b00b561466c7b40b597fc0f2514ea3773
Takahiro Mizuo <me@porkbeans.dev>
2025-04-27 05:01:30 +0900

    Fix aws autocomplete

--- exact_private_dot_rc.d/99_autocomplete_awscli.sh
+++ exact_private_dot_rc.d/99_autocomplete_awscli.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
-if has_command aws; then
+if has_command aws && has_command aws_completer; then
   case "${SHELL_NAME}" in
-    bash | zsh) eval 'complete -C /usr/bin/aws_completer aws' ;;
+    bash | zsh) eval "complete -C $(command -v aws_completer) aws" ;;
   esac
 fi
