2a4fb4c6b502b09aadc8f9898d9870909a80bfd2
Takahiro Mizuo <me@porkbeans.dev>
2026-03-22 21:27:41 +0900

    Fix step-cli autocompletion

--- exact_private_dot_rc.d/99_autocomplete_step.sh
+++ exact_private_dot_rc.d/99_autocomplete_step.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=sh
-if has_command volta; then
+if has_command step; then
   case "${SHELL_NAME}" in
     bash) eval "$(step completion "${SHELL_NAME}")" ;;
     zsh) step completion "${SHELL_NAME}" | write_only_if_diff "${HOME}/.zsh/functions/_step" ;;
